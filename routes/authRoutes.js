5a92e0303c8502c31400c1dfc324c75497b0c2bf
rfathullin <rfathullin@luxms.com>
2023-06-23 20:37:29 +0300

    fix: backend middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// registration middleware
+router.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+
+next();
+});
+
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 router.post('/signin', authController.signin);
